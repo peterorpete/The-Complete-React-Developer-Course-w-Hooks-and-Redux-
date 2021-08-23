6bc988ee20c4ccf2eb2fed27ea56258da3945e05
Pete <peterorpete@me.com>
2021-08-23 14:58:15 +0100

    end of a lesson

--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 ## Course Link
 
 https://www.udemy.com/course/react-2nd-edition/
+
+## stuff
+
+babel src/app.js --out-file=public/scripts/app.js --presets=env,react --watch
+change src/app.js
--- indescision-app/src/app.js
+++ indescision-app/src/app.js
@@ -1,10 +1,28 @@
 console.log("App.js is running!");
-
-// JSX - JavaScript XML
-var template = (
+let app = {
+  title: 'Indecision App',
+  subtitle =  'This is my subtitle',
+}
+let template = (
  <div>
-  <h1>Indecision App</h1>
-  <p>This is some info</p>
+  <h1>{app.title}</h1>
+  <p>{app.subtitle}</p>
+  <ol>
+   <li>list item 1</li>
+   <li>list item 2</li>
+  </ol>
+ </div>
+);
+let user = {
+  name: 'Peter',
+  age = 38,
+  location = 'Cornwall'
+}
+let tempateTwo = (
+ <div>
+  <h1>{user.name.toUpperCase}</h1>
+  <p>{user.age}</p>
+  <p>Location: {user.location}</p>
  </div>
 );
 var appRoot = document.getElementById("app");
