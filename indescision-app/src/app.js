961bf48f690155354880dea96b382b5d97a6af64
Pete <peterorpete@me.com>
2021-09-16 14:57:52 +0100

    add remove all and render function

--- indescision-app/src/app.js
+++ indescision-app/src/app.js
@@ -1,48 +1,50 @@
 console.log("App.js is running!");
-let app = {
+
+const app = {
  title: "Indecision App",
- subtitle: "This is my subtitle",
- options: ["One", "Two"],
-};
-let template = (
- <div>
-  <h1>{app.title}</h1>
-  {app.subtitle && <p>{app.subtitle}</p>}
-  <p>{app.options.length > 0 ? "Here are my options" : "No options"}</p>
-  <ol>
-   <li>list item 1</li>
-   <li>list item 2</li>
-  </ol>
- </div>
-);
-
-let count = 0;
-
-const addOne = () => {
- count++;
- renderCounterApp();
-};
-const minusOne = () => {
- count--;
- renderCounterApp();
-};
-const reset = () => {
- count = 0;
- renderCounterApp();
+ subtitle: "Put your life in the hands of a computer",
+ options: [],
 };
 
-let appRoot = document.getElementById("app");
+const onFormSubmit = (e) => {
+ e.preventDefault();
 
-let renderCounterApp = () => {
- const templateTwo = (
+ const option = e.target.elements.option.value;
+
+ if (option) {
+  app.options.push(option);
+  e.target.elements.option.value = "";
+  render();
+ }
+};
+
+const onRemoveAll = () => {
+ app.options = [];
+ render();
+};
+
+const appRoot = document.getElementById("app");
+
+const render = () => {
+ const template = (
   <div>
-   <h1>Count: {count}</h1>
-   <button onClick={addOne}>+1</button>
-   <button onClick={minusOne}>-1</button>
-   <button onClick={reset}>reset</button>
+   <h1>{app.title}</h1>
+   {app.subtitle && <p>{app.subtitle}</p>}
+   <p>{app.options.length > 0 ? "Here are your options" : "No options"}</p>
+   <p>{app.options.length}</p>
+   <button onClick={onRemoveAll}>Remove All</button>
+   <ol>
+    <li>Item one</li>
+    <li>Item two</li>
+   </ol>
+   <form onSubmit={onFormSubmit}>
+    <input type="text" name="option" />
+    <button>Add Option</button>
+   </form>
   </div>
  );
- ReactDOM.render(templateTwo, appRoot);
+
+ ReactDOM.render(template, appRoot);
 };
 
-renderCounterApp();
+render();
